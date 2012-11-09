40ad9d54612f6c1078e69c821813497e845d46ba
Julien Ammous <schmurfy@gmail.com>
2012-11-09 13:24:43 +0100

    added specs

--- specs/unit/carddav/resources/addressbook_collection_resource_spec.rb
+++ specs/unit/carddav/resources/addressbook_collection_resource_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../../../spec_helper'
 
 describe 'Addressbooks Collection' do
+  
+  focus "PROPFIND without body"
   before do
     @dav_ns = "DAV:"
     @carddav_ns = "urn:ietf:params:xml:ns:carddav"
@@ -58,6 +60,21 @@ describe 'Addressbooks Collection' do
       ensure_element_exists(response, %{D|response > D|href[text()="/books/first/"]})
       ensure_element_exists(response, %{D|response > D|href[text()="/books/second/"]})
     end
+    
+    
+    should 'respond correctly on PROPFIND without body' do
+      books = [
+        FactoryGirl.build(:book, path: 'first'),
+        FactoryGirl.build(:book, path: 'second')
+      ]
+      @user.expects(:addressbooks).returns(books)
+      
+      response = request(:propfind, '/books/')
+      
+      ensure_element_exists(response, %{D|href[text()="/books/first/"] + D|propstat > D|prop > D|getcontenttype[text()="httpd/unix-directory"]})
+      ensure_element_exists(response, %{D|href[text()="/books/second/"] + D|propstat > D|prop > D|getcontenttype[text()="httpd/unix-directory"]})
+    end
+    
   end
   
 end
